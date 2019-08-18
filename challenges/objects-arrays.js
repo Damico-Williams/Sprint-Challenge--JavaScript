4b14b20aac5d150673f0c1ed12a04f66c562e017
damico williams <damicowilliams@gmail.com>
2019-08-17 22:38:33 -0700

    object-arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -73,6 +73,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for(let i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university);
+}
+universities.sort()
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -82,11 +86,19 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for(let i = 0; i < graduates.length; i++){
+  contactInfo.push(graduates[i].first_name + " " + graduates[i].email)
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for(let i = 0; i < graduates.length; i++){
+  if (graduates[i].university.includes('Uni')) {
+    uni.push(graduates[i].university);
+  }
+}
 console.log(uni);
 
 
@@ -140,7 +152,7 @@ The zoos are concenred about animals with a lower population count. Find out whi
 */
 const lowerPopulation = [];
     lowerPopulation.push(zooAnimals.filter((few) => {
-      return few.population > 5;
+      return few.population < 5;
     }))
 console.log(lowerPopulation);
 
@@ -152,7 +164,7 @@ console.log(lowerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+let populationTotal = 0;
 populationTotal = zooAnimals.reduce(function(tally, value){
   return tally += value.population;
 },0)
